cd2bacc807ae9fb7b320bde79a115ab1a94554e7
qanglee <545540710@qq.com>
2023-09-29 09:54:56 +0800

    fix: name

--- packages/admin/src/pages/index.ts
+++ packages/admin/src/pages/index.ts
@@ -1,6 +1,4 @@
-import {
-  lazy
-} from "react";
+import { lazy } from "react";
 // MODERATE_AUTO_PAGE_LAZY_IMPORT:START
 const HomePage = lazy(() => import("./homePage/homePage"));
 const LoginPage = lazy(() => import("./loginPage/loginPage"));
@@ -13,70 +11,72 @@ const RolePage = lazy(() => import("./homePage/sysPage/rolePage/rolePage"));
 // 内容管理 cmsPage
 // 文章页面
 const ArticlePage = lazy(
-  () => import("./homePage/sysPage/articlePage/articlePage"),
+	() => import("./homePage/sysPage/articlePage/articlePage"),
 );
 // 图片管理
 // 页面管理
 const PageDevPage = lazy(
-  () => import("./homePage/devPage/pageDevPage/pageDevPage"),
+	() => import("./homePage/devPage/pageDevPage/pageDevPage"),
 );
 //permissionPage
 const PermissionPage = lazy(
-  () => import("./homePage/permissionPage/permissionPage"),
+	() => import("./homePage/permissionPage/permissionPage"),
 );
 //storeDevPage
 const StoreDevPage = lazy(
-  () => import("./homePage/devPage/storeDevPage/storeDevPage"),
+	() => import("./homePage/devPage/storeDevPage/storeDevPage"),
 );
 //apiDevPage
 const ApiDevPage = lazy(
-  () => import("./homePage/devPage/apiDevPage/apiDevPage"),
+	() => import("./homePage/devPage/apiDevPage/apiDevPage"),
 );
 
 ////companyPage
-const companyPage = lazy(() => import("./homePage/cmsPage/Partner/companyPage"));
+const companyPage = lazy(
+	() => import("./homePage/cmsPage/partner/companyPage"),
+);
 ////categoryPage
 const CategoryPage = lazy(
-  () => import("./homePage/cmsPage/Category/categoryPage"),
+	() => import("./homePage/cmsPage/category/categoryPage"),
 );
 ////opportunityPage
 const OpportunityPage = lazy(
-  () => import("./homePage/cmsPage/Opportunities/opportunityPage"),
+	() => import("./homePage/cmsPage/opportunities/opportunityPage"),
 );
 ////tagPage
-const TagPage = lazy(() => import("./homePage/cmsPage/Tags/tagPage"));
+const TagPage = lazy(() => import("./homePage/cmsPage/tags/tagPage"));
 ////enquiryPage
 const EnquiryPage = lazy(
-  () => import("./homePage/cmsPage/Enquiries/enquiryPage"),
+	() => import("./homePage/cmsPage/enquiries/enquiryPage"),
 );
 ////feedbackPage
 const FeedbackPage = lazy(
-  () => import("./homePage/cmsPage/MemberFeedback/feedbackPage"),
+	() => import("./homePage/cmsPage/memberFeedback/feedbackPage"),
 );
 ////paymentPage
 const PaymentPage = lazy(
-  () => import("./homePage/cmsPage/Payments/paymentPage"),
+	() => import("./homePage/cmsPage/payments/paymentPage"),
 );
-////usersPage 
-const UsersPage = lazy(() => import("./homePage/cmsPage/Members/usersPage"));
+////usersPage
+const UsersPage = lazy(() => import("./homePage/cmsPage/members/usersPage"));
 //MODERATE_AUTO_PAGE_LAZY_IMPORT:END
 export const pageList = {
-  HomePage,
-  LoginPage,
-  HelloPage,
-  UserPage,
-  RolePage,
-  ArticlePage,
-  PageDevPage,
-  PermissionPage,
-  StoreDevPage,
-  ApiDevPage,
-  companyPage,
-  CategoryPage,
-  OpportunityPage,
-  TagPage,
-  EnquiryPage,
-  FeedbackPage,
-  PaymentPage,
-  UsersPage
+	HomePage,
+	LoginPage,
+	HelloPage,
+	UserPage,
+	RolePage,
+	ArticlePage,
+	PageDevPage,
+	PermissionPage,
+	StoreDevPage,
+	ApiDevPage,
+	companyPage,
+	CategoryPage,
+	OpportunityPage,
+	TagPage,
+	EnquiryPage,
+	FeedbackPage,
+	PaymentPage,
+	UsersPage,
 };
